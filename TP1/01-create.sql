4b8348f673428072175e5960ce4ff8caccb01e64
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2021-06-22 18:02:30 -0400

    ajout dans le script

--- TP1/01-create.sql
+++ TP1/01-create.sql
@@ -1,80 +1,89 @@
+--  Nom : Carl Montpetit 
+--  Code permanent : MONC08069000 
+--
+--  Nom : Matthew Jovani
+--  Code permanent : JOVM19108705
+--
+--  Nom : Mathieu Dumont
+--  Code permanent : DUMM21059400 
+
 --Création des tables
---TODO pas certain pour les ";" et les "," + va faloir mettre les relations entre les tables etc
-create table Salaire (
-    Salaire                     char(...),  --nbr de caractères
-    Pourcentage_augmentation    char(4),    --100% = 4 = max
-    Raison_augmentation         cjar(...),  --nbr de caractères
-    primary key (Salaire);
-)
+create table Personne (
+    Nas                         number(9),    
+    Prenom                      varchar(15) not null,  
+    Nom                         varchar(15) not null,     
+    primary key (Nas),
+);
+
+create table Adresse (
+    Numero                      number(5),  
+    Rue                         varchar2(50),  
+    Code_postal                 char(6),    
+    Ville                       varchar2(50),   
+    Province                    char(2),
+    Pays                        varchar2(50),
+    foreign key (Nas) references Personne,   
+);
+
+create table Visiteur (
+    Id_visiteur                 integer(5),  
+    Nom_entreprise              varchar2(50),
+    primary key (Id_visiteur),
+    foreign key (Nas) references Personne,
+);
 
 create table Employe (
-    Id_employe                  char(...),  --nbr de caractères
-    Date_de_naissance           char(...),  --nbr de caractères
-    Poste                       char(...),  --nbr de caractères
-    Departement                 char(...),  --nbr de caractères
-    primary key (Id_employe);
-)
+    Id_employe                  integer(5),  
+    Date_de_naissance           date,     
+    Poste                       char(...),  
+    Departement                 char(...),  
+    primary key (Id_employe),
+);
 
-create Departement (
-    Id_departement              char(...),  --nbr de caractères
-    Nom_departement             char(...),  --nbr de caractères
+create table Salaire (
+    Salaire                     decimal(7,2),  
+    Pourcentage_augmentation     integer check (Pourcentage_augmentation between 1 and 100 and unique),
+    Raison_augmentation         varchar(20),   
+    primary key (Salaire),
+);
+
+create table Departement (
+    Id_departement              char(...),  
+    Nom_departement             char(...),  
     Pourcentage_attraper_covid  char(4),    --100% = 4 = max
-    primary key (Id_departement);
-)
+    primary key (Id_departement),
+);
 
-create check_in_out (
-    Date_et_heure_Entree        char(...),  --nbr de caractères
-    Date_et_heure_sortie        char(...),  --nbr de caractères
+create table check_in_out (
+    Date_et_heure_Entree        timestamp,  
+    Date_et_heure_sortie        timestamp,  
     Temperature                 char(4),    --max nombre 3 chiffres + symbole de degrée
-    Symptomes                   char(...),  --nbr de caractères
-    Risque_voisinage            char(...);  --nbr de caractères
-) 
+    Symptomes                   char(...),  
+    Risque_voisinage            char(...),  
+); 
 
-create Visiteur (
-    Id_visiteur                 char(...);  --nbr de caractères
-    -- TODO Nom_entreprise ?             char(...),
-)
-
-create Personne (
-    Nas                         char(9),    --nbr de caractères
-    Prenom                      char(...),  --nbr de caractères
-    Nom                         char(...),  --nbr de caractères   
-    primary key (Nas);
-)
-
-create Vaccin (
-    Nom_vaccin                  char(...),  --nbr de caractères
+create table Vaccin (
+    Nom_vaccin                  char(...),  
     Age_minimum                 char(3),    --max 3 chiffres
-    Date_autorisation           char(...),  --nbr de caractères
+    Date_autorisation           char(...),  
     Effets_secondaires_possibles    char(...),
-    primary key (Nom_vaccin);
-)
+    primary key (Nom_vaccin),
+);
 
---TODO On fait quoi avec Date_vaccination ??
+create table Personne_a_risque (
+    Nas                         char(9),    
+    Prenom                      char(...),  
+    Nom                         char(...),  
+    Contact_moins_2_metres      char(...),  
+    Date_actuelle               char(...),  
+    primary key (Nas),
+);
 
-create Personne_a_risque (
-    Nas                         char(9),    --nbr de caractères
-    Prenom                      char(...),  --nbr de caractères
-    Nom                         char(...),  --nbr de caractères
-    Contact_moins_2_metres      char(...),  --nbr de caractères
-    Date_actuelle               char(...),  --nbr de caractères
-    primary key (Nas);
-)
-
-create Alerte (
-    Nas                         char(9),    --nbr de caractères
-    Prenom                      char(...),  --nbr de caractères
-    Nom                         char(...),  --nbr de caractères
+create table Alerte (
+    Nas                         char(9),    
+    Prenom                      char(...),  
+    Nom                         char(...),  
     Temperature                 char(4),    --max 3 chiffres + symbole degrée
-    Date_actuelle               char(...),  --nbr de caractères
-    primary key (Nas);   
-)
-
-create Adresse (
-    Numero                      char(...),  --nbr de caractères
-    Rue                         char(...),  --nbr de caractères
-    Code_postal                 char(7),    --6 caracteres incluant un espace
-    Ville                       char(...),   --nbr de caractères
-    Province                    char(2),     --deux lettres majuscules
-    Pays                        char(...);   --nbr de caractères
-)
+    Date_actuelle               char(...),  
+    primary key (Nas),   
+);
